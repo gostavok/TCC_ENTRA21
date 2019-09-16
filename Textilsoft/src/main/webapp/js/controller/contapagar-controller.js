c15ee988dd7d17c3355aa951542bb1de4f41fc0d
Felipe <fel_dav@hotmail.com>
2019-09-16 13:39:57 -0300

    mostrando casas decimais na listagem de contas

--- Textilsoft/src/main/webapp/js/controller/contapagar-controller.js
+++ Textilsoft/src/main/webapp/js/controller/contapagar-controller.js
@@ -28,7 +28,7 @@ appTextilsoft.controller("contaPagarController", function($scope, $http) {
 	};
 
 	$scope.salvarContaPagar = function() {
-		var metodo = 'POST';
+		var metodo = 'POST';		
 		 databrasileira = $scope.contaPagar.dataVencimento;
 		 split = databrasileira.toString().split('/');
 		 novadata = split[2] + "-" +split[1]+"-"+split[0];
