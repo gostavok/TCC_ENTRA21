0f2e22eec3def3d2cfcb11189a4ebc437b356703
Arnon-T <arnon.hts@gmail.com>
2019-09-25 16:02:11 -0300

    Alterações EDIT Ordem de Serviço

--- Textilsoft/src/main/webapp/js/controller/ordem-servico-detalhe-controller.js
+++ Textilsoft/src/main/webapp/js/controller/ordem-servico-detalhe-controller.js
@@ -2,13 +2,63 @@ appTextilsoft.controller("ordemServicoDetalheController", function($scope, $http
     $routeParams) {
 
 $scope.ordemServicoDetalhe = {};
+$scope.ordemServicoDetalhe.qtdServico = "";
+$scope.ordemServicoDetalhe.servicoFornecedor = {};
+$scope.ordemServicoDetalhe.valorTotalOrdemServico = "";
+$scope.ordemServicoDetalhe.fornecedor = {};
 
-var url = 'http://localhost:8080/Textilsoft/rest/ordems-servico/';
+var url = 'http://localhost:8080/Textilsoft/rest/ordemServicos/';
+
+$scope.listarFornecedor = function(id) {
+	$http({
+		method : 'GET',
+		url : 'http://localhost:8080/Textilsoft/rest/fornecedores/'+id
+	}).then(function(response) {
+		$scope.ordemServicoDetalhe.fornecedor = response.data; 
+
+		var today = new Date();
+		var dia = today.getDate();
+		var mes = today.getMonth()+1;
+		var ano = today.getFullYear();
+		
+		$scope.dataEmissao = dia + "/" + mes + "/" + ano;
+					
+	}, function(response) {
+		console.log('error');
+		console.log(response.data);''
+		console.log(response.status);
+	});
+};
+
+$scope.listarServicoFornecedor = function(id) {
+	$http({
+		method : 'GET',
+		url : 'http://localhost:8080/Textilsoft/rest/servicosfornecedores/'+id
+	}).then(function(response) {
+		$scope.ordemServicoDetalhe.servicoFornecedor = response.data;
+		//console.log($scope.ordemServicoDetalhe.servicoFornecedor);
+	}, function(response) {
+		console.log('error');
+		console.log(response.data);
+		console.log(response.status);
+	});
+};
 
     $http.get(url + $routeParams.id).then(function(response) {
         $scope.ordemServicoDetalhe = response.data;
+        console.log(ordemServicoDetalhe.dataEntregaOrdemServico);
+        
+		var today = new Date();
+		var dia = today.getDate();
+		var mes = today.getMonth()+1;
+		var ano = today.getFullYear();
+		
+		//$scope.dataUpdate = dia + "/" + mes + "/" + ano;
+		//var dataParse = new Date(ordemServicoDetalhe.dataEntregaOrdemServico)
+		//$scope.ordemServicoDetalhe.dataEntregaOrdemServico = dataParse; 
         
         console.log('success - ordemServicoDetalheController');
+        console.log(response.data);
 
 
     }, function(response) {
@@ -44,8 +94,12 @@ var url = 'http://localhost:8080/Textilsoft/rest/ordems-servico/';
         });
     };
 
-    $scope.procuraOrdemServico = function(ordemServico) {
-        $scope.idordemServico = angular.copy(ordemServico.idOrdem);
+    $scope.procuraOrdemServico = function(ordemServicoDetalhe) {
+        $scope.idordemServico = angular.copy(ordemServicoDetalhe.idOrdem);
     }
 
+	$scope.calculoTotal = function(){
+		$scope.ordemServicoDetalhe.valorTotalOrdemServico = $scope.ordemServicoDetalhe.servicoFornecedor.valorServForn * $scope.ordemServicoDetalhe.qtdServico;	
+	};
+    
 });
