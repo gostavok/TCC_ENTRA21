20ae14e5feda5d6d987cb8baa9979cc699eaed07
felipe1445 <fel_dav@hotmail.com>
2019-09-27 18:24:17 -0300

    tela inicial modificada

--- Textilsoft/src/main/webapp/js/app.js
+++ Textilsoft/src/main/webapp/js/app.js
@@ -3,6 +3,7 @@ var appTextilsoft = angular.module("appTextilsoft", ['ngRoute']);
 appTextilsoft.config(function($routeProvider, $locationProvider){
     
 	$routeProvider
+	.when("/",{templateUrl:'view/inicio.html', controller:'mainController'})
     .when("/fornecedores-cad",{templateUrl:'view/fornecedor-cad.html', controller:'fornecedorController'})
     .when("/fornecedores-lista",{templateUrl:'view/fornecedor.html', controller:'fornecedorController'})
     .when("/fornecedores-editar/:id",{templateUrl:'view/fornecedor-edit.html', controller:'fornecedorDetalheController'})
