ded18d9f0b79506296fe468ea26a559c070f0e54
Arnon-T <arnon.hts@gmail.com>
2019-10-01 18:58:33 -0300

    Controller Detalhe

--- Textilsoft/src/main/webapp/js/controller/venda-pedido-detalhe-controller.js
+++ Textilsoft/src/main/webapp/js/controller/venda-pedido-detalhe-controller.js
@@ -113,8 +113,8 @@ appTextilsoft.controller("vendaPedidoDetalheController", function($scope,
 			url : url + vendapedido.venda.idVenda+'/'+ vendapedido.pedido.idPedido+'/'
 		}).then(function(response) {
 			
-			var atualValor = $scope.vendapedido.venda.valorTotal;
-			var valorProduto = $vendapedido.pedido.valorTotal;
+			var atualValor = vendapedido.venda.valorTotal;
+			var valorProduto = vendapedido.pedido.valorTotal;
 			var novoValor = $scope.formatNumber(atualValor) - $scope.formatNumber(valorProduto);
 			$scope.vendapedido.venda.valorTotal = $scope.formatNumber(novoValor);
 			
