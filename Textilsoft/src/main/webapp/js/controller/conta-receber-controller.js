fce5e5b04e152039f2bbb04c65b40df335fb64a7
Felipe <fel_dav@hotmail.com>
2019-10-01 21:49:20 -0300

    consulta no front end para valores a receber

--- Textilsoft/src/main/webapp/js/controller/conta-receber-controller.js
+++ Textilsoft/src/main/webapp/js/controller/conta-receber-controller.js
@@ -5,6 +5,9 @@ appTextilsoft.controller("contaReceberController", function($scope, $http,
 	$scope.contaReceber.Pago = {};
 	$scope.contaReceber.Pendente = {};
 	$scope.contaReceber.Atrasado = {};
+	$scope.pago = 0;
+	$scope.pendente = 0;
+	$scope.atrasado = 0;
 	
 	var url = 'http://localhost:8080/Textilsoft/rest/contasreceber/';
 	
@@ -26,6 +29,54 @@ appTextilsoft.controller("contaReceberController", function($scope, $http,
 
 		});
 	};
+	
+	$scope.pegarValorPago = function() {		
+		$http({
+			method : 'GET',
+			url : url + 'pago/'
+		}).then(function(response) {		
+			
+			$scope.pago = response.data;		
+			
+		}, function(response) {
+
+			console.log(response.data);
+	
+
+		});
+	};
+	$scope.pegarValorPendente = function() {		
+		$http({
+			method : 'GET',
+			url : url + 'pendente/'
+		}).then(function(response) {		
+			
+			$scope.pendente = response.data;
+			
+			
+		}, function(response) {
+
+			console.log(response.data);
+	
+
+		});
+	};
+	$scope.pegarValorAtrasado = function() {		
+		$http({
+			method : 'GET',
+			url : url + 'atrasado/'
+		}).then(function(response) {		
+			
+			$scope.atrasado = response.data;
+		
+			
+		}, function(response) {
+
+			console.log(response.data);
+	
+
+		});
+	};
 		
 	$scope.listarPorStatus = function(status) {		
 		$http({
@@ -42,6 +93,11 @@ appTextilsoft.controller("contaReceberController", function($scope, $http,
 
 		});
 	};
-			
+		$scope.chamarMetodos = function(){
+			$scope.listarStatus();
+			$scope.pegarValorPago();
+			$scope.pegarValorPendente();
+			$scope.pegarValorAtrasado();
+		}	
 
 });
